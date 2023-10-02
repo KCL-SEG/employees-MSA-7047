98bdc1f971c184fbc4e8e15bfd2c06e97e0b5ca1
Sameen Ahmed <sameenahmed7047@gmail.com>
2023-10-02 20:51:02 +0100

    Complete employee.py to show the pay of every employee object.

--- employee.py
+++ employee.py
@@ -1,31 +1,85 @@
-"""Employee pay calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 class Employee:
     def __init__(self, name):
         self.name = name
+        self.pay_components = []
 
     def get_pay(self):
-        pass
+        total_pay = sum(component.calculate() for component in self.pay_components)
+        return total_pay
 
     def __str__(self):
-        return self.name
+        components_info = " and ".join([str(component) for component in self.pay_components])
+        return f"{self.name} works {components_info}. Their total pay is {self.get_pay()}."
 
 
-# Billie works on a monthly salary of 4000.  Their total pay is 4000.
+class MonthlySalary:
+    def __init__(self, salary):
+        self.salary = salary
+
+    def calculate(self):
+        return self.salary
+
+    def __str__(self):
+        return f"on a monthly salary of {self.salary}"
+
+
+class HourlyContract:
+    def __init__(self, hours_worked, hourly_rate):
+        self.hours_worked = hours_worked
+        self.hourly_rate = hourly_rate
+        
+    def calculate(self):
+        return self.hours_worked * self.hourly_rate
+
+    def __str__(self):
+        return f"on a contract of {self.hours_worked} hours at {self.hourly_rate}/hour"
+
+
+class Commission:
+    def __init__(self, commission_count, commission_rate):
+        self.commission_count = commission_count
+        self.commission_rate = commission_rate
+
+    def calculate(self):
+        return self.commission_count * self.commission_rate
+
+    def __str__(self):
+        return f"and receives a commission for {self.commission_count} contract(s) at {self.commission_rate}/contract"
+
+
+# Billie works on a monthly salary of 4000. Their total pay is 4000.
 billie = Employee('Billie')
+billie.pay_components.append(MonthlySalary(4000))
 
-# Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
+# Charlie works on a contract of 100 hours at 25/hour. Their total pay is 2500.
 charlie = Employee('Charlie')
+charlie.pay_components.append(HourlyContract(100, 25))
 
-# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
+# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract. Their total pay is 3800.
 renee = Employee('Renee')
+renee.pay_components.append(MonthlySalary(3000))
+renee.pay_components.append(Commission(4, 200))
 
-# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
+# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract. Their total pay is 4410.
 jan = Employee('Jan')
+jan.pay_components.append(HourlyContract(150, 25))
+jan.pay_components.append(Commission(3, 220))
 
-# Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
+# Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is 3500.
 robbie = Employee('Robbie')
+robbie.pay_components.append(MonthlySalary(2000))
+robbie.pay_components.append(Commission(1, 1500))
 
-# Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
+# Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is 4200.
 ariel = Employee('Ariel')
+ariel.pay_components.append(HourlyContract(120, 30))
+ariel.pay_components.append(Commission(1, 600))
+
+# Test cases
+print(str(billie))  # Output should match the specified format
+print(str(charlie))
+print(str(renee))
+print(str(jan))
+print(str(robbie))
+print(str(ariel))
+
